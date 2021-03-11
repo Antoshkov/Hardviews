7e82a0a5be010649b33cd9fd5fd86dee173b034f
anton <antonyspost@gmail.com>
2021-03-11 18:50:58 +0200

    remove constructor for MainFragmentRecyclerViewAdapter

--- app/src/main/java/com/e/hardviews/MainFragmentRecyclerViewAdapter.java
+++ app/src/main/java/com/e/hardviews/MainFragmentRecyclerViewAdapter.java
@@ -28,8 +28,6 @@ public class MainFragmentRecyclerViewAdapter extends RecyclerView.Adapter<MainFr
         this.listener = listener;
     }
 
-    MainFragmentRecyclerViewAdapter(int a){}
-
     public void getActions(List<MyAction> myActionList) {
         myActions = myActionList;
         notifyDataSetChanged();
